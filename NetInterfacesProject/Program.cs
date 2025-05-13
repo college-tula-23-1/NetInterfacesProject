9072232db0457db791a31c2e4ff5c63818c2769b
Maxim Efimov <efimov.top.academy@gmail.com>
2025-05-13 11:27:06 +0300

    13-05-25

--- NetInterfacesProject/Program.cs
+++ NetInterfacesProject/Program.cs
@@ -1,6 +1,26 @@
 ﻿
 // ковариантность
-interface IMessanger<T>
+IMessanger<Message> outlook = new EmailMessanger();
+Message message = outlook.WriteMessage("Hello world");
+Console.WriteLine(message.Text);
+
+IMessanger<EmailMessage> bat = new EmailMessanger();
+IMessanger<Message> batClient = bat;
+Message emailMessage = batClient.WriteMessage("Hello people");
+Console.WriteLine(emailMessage.Text);
+
+// контрвариантность
+ISenderMessanger<EmailMessage> senderMessanger = new SimpleMessanger();
+senderMessanger.SendMessage(new EmailMessage("Hello Tula!"));
+
+ISenderMessanger<Message> telegram = new SimpleMessanger();
+ISenderMessanger<EmailMessage> emailClient = telegram;
+emailClient.SendMessage(new EmailMessage("Hello College!"));
+telegram.SendMessage(new Message("Hello students!"));
+
+
+// ковариантный интерфейс
+interface IMessanger<out T>
 {
     T WriteMessage(string text);
 }
@@ -13,6 +33,23 @@ class EmailMessanger : IMessanger<EmailMessage>
     }
 }
 
+
+// контрвариантный интерфейс
+interface ISenderMessanger<in T>
+{
+    void SendMessage(T message);
+}
+
+class SimpleMessanger : ISenderMessanger<Message>
+{
+    public void SendMessage(Message message)
+    {
+        Console.WriteLine($"Send message: {message.Text}");
+    }
+}
+
+
+// модель данных
 class Message
 {
     public string Text {  set; get; }

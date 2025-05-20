356e8b1597783f6ad7f53f6bbb9ae3b99d7ec023
Maxim Efimov <efimov.top.academy@gmail.com>
2025-05-20 11:46:29 +0300

    20-05-25

---
+++ NetFileSystemProject/NetFileSystemProject/Examples.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFileSystemProject
+{
+    static class Examples
+    {
+        public static void DriveInfoExample()
+        {
+            var drives = DriveInfo.GetDrives();
+
+            foreach (var drive in drives)
+            {
+                Console.WriteLine($"Drive: {drive.Name}");
+                Console.WriteLine($"Type: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Label: {drive.VolumeLabel}");
+                    Console.WriteLine($"Format: {drive.DriveFormat}");
+                    Console.WriteLine($"Total memory: {drive.TotalSize / 1024 / 1024}");
+                    Console.WriteLine($"Free memory: {drive.TotalFreeSpace / 1024 / 1024}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void DirectoryExample()
+        {
+            var currentDirPath = Directory.GetCurrentDirectory();
+            var currentDir = new DirectoryInfo(currentDirPath);
+
+            var root = currentDir.Root;
+
+            if (root is not null)
+            {
+                var dirs = root.GetDirectories();
+                //var dirs = Directory.GetDirectories(root.Name);
+                foreach (var dir in dirs)
+                    Console.WriteLine($"{dir.Name} <DIR>");
+
+                var files = root.GetFiles();
+                foreach (var file in files)
+                    Console.WriteLine($"{file.Name} {file.Extension} {file.Length}");
+
+
+                string dirNewPath = root.Name + "Maxim Directory";
+                //Directory.CreateDirectory(dirNewPath);
+                //Directory.Delete(dirNewPath, true);
+
+                DirectoryInfo dirNew = new(dirNewPath);
+                //dirNew.Create();
+                dirNew.Delete(true);
+            }
+        }
+
+        public static void FileInfoExample()
+        {
+            DirectoryInfo maxDir = new(
+                new DirectoryInfo(Directory.GetCurrentDirectory()).Root + "Maxim Directory"
+            );
+
+            if (!maxDir.Exists)
+                maxDir.Create();
+
+            File.Create(maxDir.FullName + "/file01.txt");
+
+            FileInfo file = new(maxDir.FullName + "/file02.txt");
+            file.Create();
+
+        }
+    }
+}
---
+++ NetFileSystemProject/NetFileSystemProject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+string dirPath = "H:/Maxim Directory/";
+
+string file1 = dirPath + "file01.txt";
+string file2 = dirPath + "file02.txt";
+string file3 = dirPath + "file03.txt";
+
+string fileBin1 = dirPath + "file01.bin";
+
+string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+File.WriteAllText(file3, text, Encoding.Unicode);
+
+//string[] names = ["Bobby", "Jimmy", "Sammy"];
+
+//File.WriteAllText(file1, text);
+////File.WriteAllLines(file1, names);
+//File.AppendAllLines(file1, names);
+
+
+
+
+//List<byte> bufferList = new();
+//Random random = new();
+
+//for (int i = 0; i < 10; i++)
+//    bufferList.Add((byte)random.Next(32, 255));
+
+//File.WriteAllBytes(fileBin1, bufferList.ToArray());
+
+//var lines = File.ReadAllLines(file1);
+//foreach (var line in lines)
+//    Console.WriteLine(line);
+
+//var text = File.ReadAllText(file1);
+//Console.WriteLine(text);
+
+//var buffer = File.ReadAllBytes(fileBin1);
+//foreach (var item in buffer)
+//    Console.Write($"{item} ");
+//Console.WriteLine();
+
